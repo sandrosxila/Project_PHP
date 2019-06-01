002f6e5c4e458b05ef53d17a6b50652b15c8ff73
sandrosxila <sandrosxila@yahoo.com>
2019-06-01 16:03:54 +0400

    config is corrected

--- config.sample.php
+++ config.sample.php
@@ -1,12 +1,7 @@
 <?php
-/**
- * User: TheCodeholic
- * Date: 5/5/2019
- * Time: 6:26 PM
- */
 return [
     'host' => 'localhost',
     'username' => 'root',
     'password' => '',
-    'database' => 'custom_framework'
+    'database' => 'project'
 ];
