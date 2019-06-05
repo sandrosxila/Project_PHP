7fec60bfe989ba8b8f507aec7f4f48c81af49fc6
sandrosxila <sandrosxila@yahoo.com>
2019-06-05 07:17:48 +0400

    added sample textes

--- public/compile.js
+++ public/compile.js
@@ -1,3 +1,50 @@
+var scripts = new Map([
+    ['C++', '#include <iostream>\n' +
+    '\n' +
+    'using namespace std;\n' +
+    '\n' +
+    'int main() {\n' +
+    '\tint x=10;\n' +
+    '\tint y=25;\n' +
+    '\tint z=x+y;\n' +
+    '\n' +
+    '\tcout<<"Sum of x+y = " << z;\n' +
+    '}\n'],
+
+    ['NodeJS', 'var x = 10;\n' +
+    'var y = 25;\n' +
+    'var z = x + y;\n' +
+    '\n' +
+    'console.log("x + y = " + z);\n'],
+
+    ['Python 3', 'x=10;\n' +
+    'y=25;\n' +
+    'z=x+y;\n' +
+    'print ("sum of x+y =", z);\n'],
+
+    ['Java', 'public class MyClass {\n' +
+    '    public static void main(String args[]) {\n' +
+    '        int x=10;\n' +
+    '        int y=25;\n' +
+    '        int z=x+y;\n' +
+    '\n' +
+    '        System.out.println("Sum of x+y = " + z);\n' +
+    '    }\n' +
+    '}\n'],
+
+    ['PHP', '<?php\n' +
+    '\n' +
+    '    $x=10;\n' +
+    '    $y=25;\n' +
+    '    $z=$x+$y;\n' +
+    '\n' +
+    '    $msg = \'Sum of x+y = \';\n' +
+    '\n' +
+    '    print($msg.$z);\n' +
+    '\n' +
+    '?>'],
+]);
+
 var editor = ace.edit("editor");
 var PythonMode = ace.require("ace/mode/python").Mode;
 var JavaScriptMode = ace.require("ace/mode/javascript").Mode;
@@ -40,10 +87,15 @@ function convert(str)
 
 $('#choose_lang  option').click(function() {
     var txt = $('#choose_lang option:selected').text();
-    console.log(txt);
+//    console.log(txt,scripts.get(txt));
     $('#checker').val( "1" );
     $('#lang').val($('#choose_lang option:selected').val());
+    if(editor.getValue()=='') {
+        editor.setValue("");
+        editor.session.insert(editor.getCursorPosition(), scripts.get(txt));
+    }
     if(txt=='C++'){
+
         editor.session.setMode(new CppMode());
     }
     else if(txt=='NodeJS'){
--- views/about.php
+++ views/about.php
@@ -1,4 +1,5 @@
 <div>
     <h1>About us</h1>
-    <h5>Compiler Uses Jdoodle API</h5>
+    <h3>Compiler Uses Jdoodle API</h3>
+    <h5>Created By Sandro Skhirtladze</h5>
 </div>
